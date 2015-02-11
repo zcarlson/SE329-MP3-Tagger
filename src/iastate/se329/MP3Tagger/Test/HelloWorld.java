ea95d961391b476bb56e94a89206fe7bde1f6685
Trey Erenberger <treye22@gmail.com>
2015-02-11 15:00:50 -0600

    Added mp3 tag library
    added mp3 tag library and test song from last.fm

--- src/iastate/se329/MP3Tagger/Test/HelloWorld.java
+++ src/iastate/se329/MP3Tagger/Test/HelloWorld.java
@@ -1,15 +1,23 @@
 package iastate.se329.MP3Tagger.Test;
 
+import java.io.IOException;
 import java.util.List;
+
+import com.mpatric.mp3agic.*;
 
 import fm.last.musicbrainz.data.model.*;
 import fm.last.musicbrainz.data.dao.impl.*;
 import fm.last.musicbrainz.data.hibernate.*;
 
+
 public class HelloWorld {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws UnsupportedTagException, InvalidDataException, IOException
 	{
+		
+		Mp3File file = new Mp3File("Death Grips - Get Got.mp3");
+		
+		System.out.println(file.getLength());
 		/*
 		Artist artist = new Artist();
 		ArtistDaoImpl artistdao = new ArtistDaoImpl();
