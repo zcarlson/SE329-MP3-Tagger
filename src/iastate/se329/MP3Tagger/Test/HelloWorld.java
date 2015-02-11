90ebe08b3abfcda891dcc0dd64ee124f6b87109e
Trey Erenberger <treye22@gmail.com>
2015-02-11 15:24:31 -0600

    Commented brief test code

--- src/iastate/se329/MP3Tagger/Test/HelloWorld.java
+++ src/iastate/se329/MP3Tagger/Test/HelloWorld.java
@@ -14,9 +14,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) throws UnsupportedTagException, InvalidDataException, IOException
 	{
-		
+		//file on system, in the project root folder. Need to experiment with getting paths right
 		Mp3File file = new Mp3File("Death Grips - Get Got.mp3");
+		//Extract tag from mp3 file
 		ID3v2 id = file.getId3v2Tag();
+		//get various attributes from tag, some potentially not populated which will return null
 		System.out.println(id.getArtist() + " - " + id.getAlbum());
 		System.out.println(file.getFilename() + " " + file.getLengthInSeconds() + " ");
 		/*
