3a6c87ae617b4dabda3b0eb69cf629e33c9cb171
Trey Erenberger <treye22@gmail.com>
2015-02-11 14:21:40 -0600

    Added MusicBrainz java API jar and references

--- src/iastate/se329/MP3Tagger/Test/HelloWorld.java
+++ src/iastate/se329/MP3Tagger/Test/HelloWorld.java
@@ -1,10 +1,23 @@
 package iastate.se329.MP3Tagger.Test;
 
+import java.util.List;
+
+import fm.last.musicbrainz.data.model.*;
+import fm.last.musicbrainz.data.dao.impl.*;
+import fm.last.musicbrainz.data.hibernate.*;
+
 public class HelloWorld {
 
 	public static void main(String[] args)
 	{
-		System.out.println("hey");
+		/*
+		Artist artist = new Artist();
+		ArtistDaoImpl artistdao = new ArtistDaoImpl();
+		List<Artist> artistList = artistdao.getByName("Beck");
+		artist = artistList.get(0);
+		System.out.println(artist.getName());
+		*/
+		System.out.println("se");
 	}
 	
 }
