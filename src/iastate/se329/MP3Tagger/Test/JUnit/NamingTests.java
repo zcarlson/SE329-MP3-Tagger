590250820a28f8acc8c25a6a90637b4d651028e2
aore <aore@krellinst.org>
2015-04-29 19:00:43 -0500

    Add whitebox testing file

---
+++ src/iastate/se329/MP3Tagger/Test/JUnit/NamingTests.java
@@ -0,0 +1,13 @@
+package iastate.se329.MP3Tagger.Test.JUnit;
+
+import org.junit.Test;
+
+
+// White-box testing
+public class NamingTests {
+
+	@Test
+	public void TestAllCombinations() {
+		
+	}
+}
